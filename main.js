2ac2d7e6707b4809247e9894aa3930a6a45e0df3
Santiago Fernández <santiagofernandez@MacBook-Air-de-Santiago.local>
2023-07-18 22:49:41 -0300

    primer commit elixir

---
+++ main.js
@@ -0,0 +1,26 @@
+
+
+
+function hacerPregunta (){
+    let pregunta = prompt(`
+    ¿Qué bebida vas a querer?
+    1: Cerveza
+    2: Vino
+    3: johnnie walker blue label
+    4: Gin Tonic`);
+    
+    if (pregunta == 3){
+        alert ("Es un elixir, el otro es un whisky. Uno se toma, el otro se saborea.");
+    } else {
+        alert(`Es una buena elección.`)
+    }
+    
+    if (condition) {
+        
+    } else {
+        
+    }
+
+}
+
+hacerPregunta()
